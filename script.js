8af0b42d339d87489c1c9cc5d15b4f5d56f51e57
Imron <Umurzakov117ali@gmail.com>
2021-01-04 01:11:25 +0500

    initial

---
+++ script.js
@@ -0,0 +1,22 @@
+var nick = prompt('Ismingizni kiriting')   
+alert('Hush kelibsiz ' + nick)
+console.log( 'Mening ismim ' + nick);
+
+var old = prompt('Yoshingiz nechida?')
+console.log('Men ' + old + ' yoshdaman');
+
+var misol1 = prompt('Misolni yeching 6 + 3=?')
+console.log('Misol 1: 6 + 3 = 9 Sizning javob ' + misol1);
+
+var misol2 = prompt('Misolni yeching 20 - 5=?')
+console.log('Misol 2: 20 - 5 = 15 Sizning javob ' + misol2);
+
+var misol3 = prompt('Misolni yeching 15 * 2=?')
+console.log('Misol 3: 15 * 2 = 30 Sizning javob ' + misol3);
+
+var misol4 = prompt('Misolni yeching 8 / 4=?')
+console.log('Misolni 4: 8 / 4 = 2 Sizning javob ' + misol4);
+
+var misol5 = prompt('Misolni yeching 10 % 3 =?')
+console.log('Misol 5: 10 % 3 =1 Sizning javob ' + misol5);
+alert('natijani ko`rish uchun consolga kiring ')
